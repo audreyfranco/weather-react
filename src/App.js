cacdb24d4ed745d1d76651fb7322cc2042135439
audreyfranco <74529775+audreyfranco@users.noreply.github.com>
2021-02-19 16:58:23 -0800

    temperature api update

--- src/App.js
+++ src/App.js
@@ -9,13 +9,7 @@ export default function App() {
     <div className="App">
       <div className="weather">
         <Search />
-        <div className="Row">
-          <div className="col-6">
-            <h1 id="city">Los Angeles</h1>
-            <h2>Sunday, January 17, 2021 13:30</h2>
-          </div>
-          <Temperature />
-        </div>
+        <Temperature defaultCity = "Los Angeles" />
         <Forecast />
       </div>
     </div>
--- src/Temperature.js
+++ src/Temperature.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
-export default function Temperature() {
+export default function Temperature(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+function handleResponse(response) {
+setWeatherData({
+  ready: true,
+  weather: response.data.main.temp,
+  humidity: response.data.main.humidity,
+  date: "February 16",
+  description: response.data.weather[0].description,
+  iconUrl: "http://openweathermap.org/img/wn/01d@2x.png",
+  wind: response.data.wind.speed,
+  city: response.data.name
+});
+
+}
+if (weatherData.ready) {
   return (
+    <div className="row">
+    <div className="col-6">
+            <h1 id="city">{weatherData.city}</h1>
+            <h2>{weatherData.date} 13:30</h2>
+          </div>
     <div className="col-6">
       <div className="current">
         <img
-          src="http://openweathermap.org/img/wn/01d@2x.png"
-          alt="Icon"
+          src={weatherData.iconUrl}
+          alt={weatherData}
           id="icon"
           className="float-left"
         />
         <span className="temperature" id="temperature">
-          27
+          {Math.round(weatherData.weather)}
         </span>
         <span className="units">
           <a href="#" id="celsius-link" className="active">
@@ -26,15 +47,24 @@ export default function Temperature() {
       </div>
       <div className="details">
         <ul>
-          <li id="description">Clear</li>
+          <li id="description" className="text-capitalized">{weatherData.description}</li>
           <li>
-            Humidity: <span id="humidity"></span>%
+            Humidity: <span id="humidity">{weatherData.humidity}</span>%
           </li>
           <li>
-            Wind: <span id="wind"></span>km/h
+            Wind: <span id="wind">{weatherData.wind}</span>km/h
           </li>
         </ul>
       </div>
     </div>
+    </div>
   );
+} else {
+  const apiKey = "8402b695ede9a6c63a7ea98262105a24";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
+  return "loading...";
 }
+}
+
